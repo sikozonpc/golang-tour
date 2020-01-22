ed6328399aad7264d6a75be98ca99fcfe5c9689a
Tiago Ferreira <sikozonbatata@gmail.com>
2020-01-22 22:55:51 +0000

    finished quiz game

--- gophercises/quiz_game/quiz_game.go
+++ gophercises/quiz_game/quiz_game.go
@@ -5,39 +5,53 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"strings"
+	"time"
 )
+
+// Problem : Problem struct
+type Problem struct {
+	Question string
+	Answer   string
+}
 
 func main() {
 	var score int
 	// Use flags package to show cli options
-	csvFile := readArguments()
+	csvFile, timeLimit := readArguments()
 
 	// Read the csv
-	questions := make(map[string]string)
-	questions = createQuestionsMap(*csvFile) // get the value from the pointer
+	lines := readCsvFile(*csvFile) // get the value from the pointer
+	problems := parseLines(lines)
 
-	// Print each question
-	reader := bufio.NewReader(os.Stdin)
-	for question := range questions {
-		fmt.Printf("Question: %s= ?\n", question)
+	fmt.Printf("All settled for a %v seconds Quiz with %d problems...\n", *timeLimit, len(problems))
+	// User input to start the quiz
+	var ready string
+	fmt.Scanf("Are you ready ? ", &ready)
 
-		// Wait for user input
-		answer, _ := reader.ReadString('\n')
+	if ready == "OK" {
+		timer := time.NewTimer(*timeLimit * time.Second)
 
-		// Validate the input
-		if strings.Compare(strings.Trim(strings.ToLower(answer), "\n"), questions[question]) == 0 {
-			score++
+		// Print each question
+		reader := bufio.NewReader(os.Stdin)
+		for _, problem := range problems {
+			fmt.Printf("Question: %s= ?\n", problem.Question)
+
+			// Wait for user input
+			answer, _ := reader.ReadString('\n')
+
+			// Validate the input
+			if strings.Compare(strings.Trim(strings.ToLower(answer), "\n"), problem.Answer) == 0 {
+				score++
+			}
 		}
-	}
 
-	fmt.Printf("\nYOUR SCORE: %d\n", score)
+		fmt.Printf("\nYOUR SCORE: %d\n", score)
+	}
 }
 
-func createQuestionsMap(filePath string) map[string]string {
-	questions := make(map[string]string)
+func readCsvFile(filePath string) [][]string {
 	// Load the csv file
 	raw, err := os.Open(filePath)
 
@@ -47,29 +61,30 @@ func createQuestionsMap(filePath string) map[string]string {
 
 	// Create a new reader
 	reader := csv.NewReader(raw)
-	for {
-		line, err := reader.Read()
+	lines, err := reader.ReadAll()
 
-		if len(line) >= 1 {
-			questions[line[0]] = line[1]
-		}
-
-		// Stop at EOF.
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			panic(err)
-		}
+	if err != nil {
+		panic(err)
 	}
 
-	return questions
+	return lines
 }
 
-func readArguments() *string {
+func readArguments() (*string, *int) {
 	csvFile := flag.String("csv", "questions.csv", "CSV file with the question for the quiz, they must be (question, value) formated.")
+	timer := flag.Int("timer", 30, "Time limit for the quiz.")
 
 	flag.Parse()
 
-	return csvFile
+	return csvFile, timer
+}
+
+func parseLines(lines [][]string) []Problem {
+	problems := make([]Problem, len(lines))
+
+	for i, line := range lines {
+		problems[i] = Problem{line[0], line[1]}
+	}
+
+	return problems
 }
